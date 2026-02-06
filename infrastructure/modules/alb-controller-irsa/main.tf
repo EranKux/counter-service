f9d5a91f3e837138d2c4e381cf73b17ecf010cac
EranKux <kux100@gmail.com>
2026-02-06 20:00:14 +0200

    Test commit

--- infrastructure/modules/alb-controller-irsa/main.tf
+++ infrastructure/modules/alb-controller-irsa/main.tf
@@ -76,6 +76,7 @@ resource "aws_iam_policy" "alb_controller" {
           "elasticloadbalancing:DescribeLoadBalancers",
           "elasticloadbalancing:DescribeLoadBalancerAttributes",
           "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeListenerAttributes",
           "elasticloadbalancing:DescribeListenerCertificates",
           "elasticloadbalancing:DescribeSSLPolicies",
           "elasticloadbalancing:DescribeRules",
