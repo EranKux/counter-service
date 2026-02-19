8c9b729b6dfde2b4bd82d988b1b411136943ce0c
EranKux <kux100@gmail.com>
2026-02-19 09:08:42 +0200

    fix: upgrade Terraform to 1.10.0 to support use_lockfile in backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-eu-west-1-eran"
     key            = "terraform/state"
     region         = "eu-west-1"
-    use_lockfile   = true
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
